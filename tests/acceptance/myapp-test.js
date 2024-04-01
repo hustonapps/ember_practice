1b9731409e106913a7d7dc975ed5181e7990037e
Matt Huston <hustonapps@gmail.com>
2024-03-31 19:49:59 -0600

    components with classes

--- tests/acceptance/myapp-test.js
+++ tests/acceptance/myapp-test.js
@@ -9,6 +9,8 @@ module('Acceptance | myapp', (hooks) => {
     await visit('/');
 
     assert.strictEqual(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -21,6 +23,8 @@ module('Acceptance | myapp', (hooks) => {
     await visit('/about');
 
     assert.strictEqual(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals');
     assert.dom('.jumbo a.button').hasText('Contact Us');
     await click('.jumbo a.button');
@@ -32,11 +36,31 @@ module('Acceptance | myapp', (hooks) => {
     await visit('/getting-in-touch');
 
     assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('Home');
     await click('.jumbo a.button');
 
+    assert.strictEqual(currentURL(), '/');
+  });
+
+  test('navigating using the navbar', async (assert) => {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.strictEqual(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
     assert.strictEqual(currentURL(), '/');
   });
 });
